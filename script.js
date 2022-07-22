d9dec2f7875e982cac8a4fd7d59bf3bc82a8abb6
ccama22 <ccamacruz2018@gmail.com>
2022-07-21 20:18:45 -0500

    actual

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  fetchData();
+})
+const fetchData = async () => {
+  try {
+    const res = await fetch("api.json");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+console.log("data object", fetchData.);
