c818b90e1accb52f827d8bb623a5a4958e81e51b
ccama22 <ccamacruz2018@gmail.com>
2022-07-24 16:15:57 -0500

    actual tienda3

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const cards = document.getElementById("cards");
 const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
 const templateCart = document.getElementById("content-data").content;
+const footer = document.getElementById("footer-cart");
+const templateFooter = document.getElementById("template-footer").content;
 const fragment = document.createDocumentFragment();
 let cart = {};
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchData();
+  if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+    pintarCart();
+  }
 });
 
 cards.addEventListener("click", (e) => {
   addCart(e);
+});
+
+items.addEventListener("click", (e) => {
+  btnAction(e);
 });
 
 const fetchData = async () => {
@@ -33,6 +43,9 @@ const pintarCards = (data) => {
     templateCard
       .querySelector("img")
       .setAttribute("src", producto.image.thumbnail);
+    templateCard
+      .querySelector(".card-imag-disable")
+      .setAttribute("src", producto.image.thumbnail);
 
     templateCard.querySelector(".btn-dark").dataset.id = producto.id;
     const clone = templateCard.cloneNode(true);
@@ -44,6 +57,7 @@ const pintarCards = (data) => {
 const addCart = (e) => {
   console.log(e.target.classList.contains("btn-dark"));
   if (e.target.classList.contains("btn-dark")) {
+    console.log("imagen", e.target.parentElement);
     setCart(e.target.parentElement);
   }
   //detener cualquier otro evento que se pueda generar en nuestro item , porque se podria heredar eventos del contenedor padre
@@ -51,10 +65,11 @@ const addCart = (e) => {
 };
 
 const setCart = (objeto) => {
-  console.log(objeto);
+  console.log("objeto ccama", objeto);
   const products = {
     id: objeto.querySelector(".btn-dark").dataset.id,
     // imag: objeto.querySelector(".card-img-top").textContent,
+    imag: objeto.querySelector(".card-imag-disable").getAttribute("src"),
     title: objeto.querySelector(".content-title").textContent,
     description: objeto.querySelector(".content-descripcion").textContent,
     price: objeto.querySelector(".content-price").textContent,
@@ -70,10 +85,11 @@ const setCart = (objeto) => {
 
 const pintarCart = () => {
   items.innerHTML = "";
-  console.log(cart);
+  console.log("carrito maximo", cart);
   Object.values(cart).forEach((producto) => {
     // templateCart.querySelector(".img-data").textContent = producto.imag;
     console.log(templateCart);
+    console.log("ccama cruz", producto);
     templateCart
       .querySelector(".content-data-text")
       .querySelector(".content-title-data").textContent = producto.title;
@@ -88,9 +104,50 @@ const pintarCart = () => {
       .querySelector(".cart-data-quantity").textContent = producto.quantity;
     templateCart
       .querySelector(".content-data-price")
-      .querySelector(".content-price").textContent = producto.price;
+      .querySelector(".content-price").textContent = `$${
+      producto.quantity * producto.price.slice(1)
+    }`;
+    //botones
+    templateCart.querySelector(".btn-sum").dataset.id = producto.id;
+    templateCart.querySelector(".btn-rest").dataset.id = producto.id;
+
+    templateCart.querySelector("img").setAttribute("src", producto.imag);
     const clone = templateCart.cloneNode(true);
     fragment.appendChild(clone);
   });
   items.appendChild(fragment);
+  paintFooterCart();
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const paintFooterCart = () => {
+  footer.innerHTML = "";
+  const nPriceCart = Object.values(cart).reduce(
+    (acc, { quantity, price }) => acc + quantity * price.slice(1),
+    0
+  );
+  console.log("ccama precio final", nPriceCart);
+  templateFooter.querySelectorAll("p")[1].textContent = nPriceCart.toFixed(2);
+  const clone = templateFooter.cloneNode(true);
+  fragment.appendChild(clone);
+  footer.appendChild(fragment);
+};
+const btnAction = (e) => {
+  console.log("cccc", e.target);
+  if (e.target.classList.contains("btn-sum")) {
+    console.log(cart[e.target.dataset.id]);
+    const products = cart[e.target.dataset.id];
+    products.quantity = cart[e.target.dataset.id].quantity + 1;
+    cart[e.target.dataset.id] = { ...products };
+    pintarCart();
+  }
+  if (e.target.classList.contains("btn-rest")) {
+    const products = cart[e.target.dataset.id];
+    products.quantity--;
+    if (products.quantity === 0) {
+      delete cart[e.target.dataset.id];
+    }
+    pintarCart();
+  }
+  e.stopPropagation();
 };
